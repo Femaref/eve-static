3a89964d7d4870f00f0db4836384e2ab61ac42eb
Femaref <femaref@googlemail.com>
2013-07-05 21:46:01 +0200

    some often used lookups

--- lib/eve_static/queries/basic.rb
+++ lib/eve_static/queries/basic.rb
@@ -16,6 +16,35 @@ module EveStatic
         
         instance[:invTypes].where(:typeID => typeID).select(Sequel.lit('typeName')).first[:typeName]
       end
+      
+      def groupName(groupID)
+        instance[:invGroups].where(:groupID => groupID).select(Sequel.lit('groupName')).first[:groupName]
+      end
+      
+      def groupID(groupName)
+        instance[:invGroups].where(:groupName => groupName).select(Sequel.lit('groupID')).first[:groupID]
+      end
+      
+      def typeGroup(type)
+        typeID = coerce_type_id(type)
+        
+        instance[:invTypes].inner_join(:invGroups, :groupID => :groupID).where(:typeID => typeID).select_all(:invGroups).first
+      end
+      
+      def categoryName(categoryID)
+        instance[:invCategories].where(:categoryID => categoryID).select(Sequel.lit('categoryName')).first[:categoryName]
+      end
+      
+      def categoryID(categoryName)
+        instance[:invCategories].where(:categoryName => categoryName).select(Sequel.lit('categoryID')).first[:categoryID]
+      end
+      
+      def typeCategory(type)
+        typeID = coerce_type_id(type)
+        
+        instance[:invTypes].inner_join(:invGroups, :groupID => :groupID).inner_join(:invCategories, :categoryID => :categoryID).where(:typeID => typeID).select_all(:invCategories).first
+      end
+      
     end
   end
 end
